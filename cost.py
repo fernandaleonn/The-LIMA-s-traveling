0a0ef5689fe767673c6138b047144c79baf0b0b1
Fer León <141685549+fernandaleonn@users.noreply.github.com>
2023-12-04 12:38:37 -0600

    Delete cost.py

--- cost.py
+++
@@ -1,5 +0,0 @@
-def cost(cities, matrix_distance, route):
-    total_dist = 0
-    for i in range(len(route) - 1):
-        total_dist += matrix_distance[route[i]][route[i + 1]]
-    return total_dist
