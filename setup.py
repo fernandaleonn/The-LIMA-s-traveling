e0fb014eb551b86c7a95c90b00f328cd9278c379
Fer León <141685549+fernandaleonn@users.noreply.github.com>
2023-12-04 12:01:27 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='minimal',
+      version='0.1',
+      description='A Python package for something',
+      url='http://github.com/rvf0068/minimal',
+      author='Yo Mero',
+      author_email='yomero@gmail.com',
+      license='MIT',
+      packages=['minimal'],
+      zip_safe=False)
