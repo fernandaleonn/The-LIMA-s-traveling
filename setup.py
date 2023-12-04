64d982ad859a54e91f466356bcc542e56e6e4cd3
Fer León <141685549+fernandaleonn@users.noreply.github.com>
2023-12-04 12:04:31 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='minimal',
+setup(name='The travelling LIMA's',
       version='0.1',
       description='A Python package for something',
-      url='http://github.com/rvf0068/minimal',
-      author='Yo Mero',
-      author_email='yomero@gmail.com',
+      url='https://github.com/fernandaleonn/CUADRADOSLATINOS',
+      author="Fernanda León,Eduardo Perez,Eduardo Alanis,Alejandro Sanchez",
+      author_email="ga351962@uaeh.edu.mx,sa374709@uaeh.edu.mx,pe439921@uaeh.edu.mx,al440184@uaeh.edu.mx",
       license='MIT',
-      packages=['minimal'],
+      packages=['THETRAVELINGSALESMANPROBLEM'],
       zip_safe=False)
