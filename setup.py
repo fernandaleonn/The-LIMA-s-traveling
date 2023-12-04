6ea6dedc85bb0d272e05312406e57ad6be8af496
Fer León <141685549+fernandaleonn@users.noreply.github.com>
2023-12-04 12:28:31 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='The travelling LIMA's',
+setup(name='travelpy',
       version='0.1',
       description='A Python package for develop a travelling sales man ´ s code',
       url='https://github.com/fernandaleonn/CUADRADOSLATINOS',
       author="Fernanda León,Eduardo Perez,Eduardo Alanis,Alejandro Sanchez",
       author_email="ga351962@uaeh.edu.mx,sa374709@uaeh.edu.mx,pe439921@uaeh.edu.mx,al440184@uaeh.edu.mx",
       license='MIT',
-      packages=['THETRAVELINGSALESMANPROBLEM'],
+      packages=['travelpy'],
       zip_safe=False)
