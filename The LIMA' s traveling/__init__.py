e7d6884be8186077b8c3168ed361fda31821b391
Fer León <141685549+fernandaleonn@users.noreply.github.com>
2023-12-04 12:08:59 -0600

    Add files via upload

---
+++ The LIMA' s traveling/__init__.py
@@ -0,0 +1,14 @@
+def my_sum(x, y):
+    """A function that sums. """
+    return x+y
+
+
+def my_mul(x, y):
+    """A function that multiply. """
+    return x*y
+
+
+def fer_rest(x,y):
+    """Una función que resta. """
+    return x-y
+    
