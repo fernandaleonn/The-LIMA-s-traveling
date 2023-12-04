1db5efec9a009b53b5132e7a5fa90a10882c0df9
Alejandro Sánchez <141791463+alesac12@users.noreply.github.com>
2023-12-04 12:08:02 -0600

    Update setup.py
    Add description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='The travelling LIMA's',
       version='0.1',
-      description='A Python package for something',
+      description='A Python package for develop a travelling sales mans code',
       url='https://github.com/fernandaleonn/CUADRADOSLATINOS',
       author="Fernanda León,Eduardo Perez,Eduardo Alanis,Alejandro Sanchez",
       author_email="ga351962@uaeh.edu.mx,sa374709@uaeh.edu.mx,pe439921@uaeh.edu.mx,al440184@uaeh.edu.mx",
