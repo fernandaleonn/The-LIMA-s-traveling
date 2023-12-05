59e53744a922e600c6804146083e9e3cde8b48c1
Alejandro Sánchez <141791463+alesac12@users.noreply.github.com>
2023-12-04 21:46:39 -0600

    Update salesman_traveling.py

--- travelpy/salesman_traveling.py
+++ travelpy/salesman_traveling.py
@@ -1,3 +1,16 @@
+from itertools import permutations
+
+def city_permutation(cities):
+    all_permutations = permutations(range(1, cities))  # Genera todas las permutaciones posibles excepto (0, ...)
+    for permutation in all_permutations:
+        yield (0,) + permutation + (0,)  # Agrega 0 al inicio y al final de la permutación
+
+def cost(cities, matrix_distance, route):
+    total_dist = 0
+    for i in range(len(route) - 1):
+        total_dist += matrix_distance[route[i]][route[i + 1]]
+    return total_dist
+
 def salesman_traveling(cities, matrix_distance):
     OptC = float('inf')  # Inicializar con un valor alto para comparación
     OptX = []
