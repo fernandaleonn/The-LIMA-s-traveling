d76961a312737a3bb115d98201cf5c8ef20f2c55
Eduardo Alanis <141802122+Eduardo-Alanis-Garcia@users.noreply.github.com>
2023-12-06 15:57:53 -0600

    Update matrix_generator.py

--- travelpy/matrix_generator.py
+++ travelpy/matrix_generator.py
@@ -1,13 +1,9 @@
 import numpy as np
 
 def matrix_generator(n):
-    # Generar una matriz aleatoria 20x20 de enteros entre 1 y 50
+     """Generate a matrix representing the distance between cities."""
     random_matrix = np.random.randint(1, 50, size=(n, n))
-
-    # Establecer la diagonal en ceros
     np.fill_diagonal(random_matrix, 0)
-
-    # Hacer la matriz simétrica
     symmetric_matrix = (random_matrix + random_matrix.T) // 2
     return symmetric_matrix.tolist()
 
