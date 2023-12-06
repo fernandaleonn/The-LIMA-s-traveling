29ee3895f52d9423b485ac7811ce08c2200fceab
Alejandro Sánchez <141791463+alesac12@users.noreply.github.com>
2023-12-06 14:52:17 -0600

    Update cost.py

--- travelpy/cost.py
+++ travelpy/cost.py
@@ -1,4 +1,4 @@
-def cost(cities, matrix_distance, route):
+def cost(matrix_distance, route):
     total_dist = 0
     for i in range(len(route) - 1):
         total_dist += matrix_distance[route[i]][route[i + 1]]
