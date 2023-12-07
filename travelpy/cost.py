e6b393548d523524c9150db8500beac9fe4ed7db
Alejandro Sánchez <141791463+alesac12@users.noreply.github.com>
2023-12-06 22:12:23 -0600

    Update cost.py

--- travelpy/cost.py
+++ travelpy/cost.py
@@ -1,6 +1,6 @@
 
 def cost(matrix_distance, route):
-    """Gives you the prices of all the routes that we have generated in city permutations."""
+    """Gives you the prices of all the routes that we have generated in city permutations"""
     total_dist = 0
     for i in range(len(route) - 1):
         total_dist += matrix_distance[route[i]][route[i + 1]]
