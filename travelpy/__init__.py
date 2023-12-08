52a317457071a53b97d43e8ecacadeee41ed3cdf
Alejandro Sánchez <141791463+alesac12@users.noreply.github.com>
2023-12-07 20:23:55 -0600

    Update __init__.py

--- travelpy/__init__.py
+++ travelpy/__init__.py
@@ -5,3 +5,4 @@ from travelpy.salesman_traveling import *
 from travelpy.matrix_generator import *
 from travelpy.graph_generator import *
 from travelpy.salesman_traveling_with_pruning import *
+from travelpy.matrix_latex import *
