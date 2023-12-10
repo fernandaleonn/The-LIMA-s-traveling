5b31ca1c036561815bd0433d332197b18eab7049
Alejandro Sánchez <141791463+alesac12@users.noreply.github.com>
2023-12-09 23:29:27 -0600

    Update city_permutation.py

--- travelpy/city_permutation.py
+++ travelpy/city_permutation.py
@@ -1,7 +1,11 @@
+"""
+This module provides a function to generate permutations of cities to be traveled.
+"""
+
 from itertools import permutations
 
 def city_permutation(cities):
     """Function that generates all the permutations of the cities to be traveled"""
-    all_permutations = permutations(range(1, cities)) 
+    all_permutations = permutations(range(1, cities))
     for permutation in all_permutations:
         yield (0,) + permutation + (0,)
